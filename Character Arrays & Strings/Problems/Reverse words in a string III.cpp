519c0e631b18e7a23d32109645ca66c374bff01f
Keshav Chandra Ray <keshavray1129@gmail.com>
2022-06-20 12:03:13 +0530

    Reverse words in a string III

---
+++ Character Arrays & Strings/Problems/Reverse words in a string III.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string reverseWords(string s) {
+        int l=0,r=0;
+        string ans="";
+        while(l<s.size()){
+            while(r<s.size() && s[r] != ' ')   
+                r++;
+
+            reverse(s.begin()+l,s.begin()+r);
+
+            l=r+1;
+            r=l;
+        }
+        
+        return s;
+    }
+
+int main(){
+    string s="My name is Obito";
+    reverseWords(s);
+    cout<<reverseWords(s)<<endl;
+
+    return 0;
+}
